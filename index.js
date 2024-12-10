05cb24bf6b5bc653f0a64a44cb277e55836913bb
Ronaldo Kipkirui <ronaldochirchir8@gmail.com>
2024-12-10 20:08:08 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    let thankYouCards = [];
+    for (let i = 0; i < names.length; i++) {
+      thankYouCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankYouCards;
+  }
+  console.log(writeCards(["Alice", "Bob"], "birthday"));
+  [
+    "Thank you, Alice, for the wonderful birthday gift!",
+    "Thank you, Bob, for the wonderful birthday gift!"
+  ]
+  function countDown(number) {
+    let count = number;
+    while (count >= 0) {
+      console.log(count);
+      count--;
+    }
+  }
+  
